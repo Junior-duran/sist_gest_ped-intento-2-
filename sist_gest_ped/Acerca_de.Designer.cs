0ee7f8376a6a07358a5ee43e33a9c0009b602832
Junior-duran <juniordelarosa35@outlook.es>
2025-04-10 18:03:07 -0400

    Se termino el proyecto.

--- sist_gest_ped/Acerca_de.Designer.cs
+++ sist_gest_ped/Acerca_de.Designer.cs
@@ -29,33 +29,62 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.FromArgb(64, 64, 64);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(33, 19);
+            button1.Location = new Point(38, 22);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 26);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 0;
             button1.Text = "Volver";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(378, 228);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 28);
+            label1.TabIndex = 1;
+            label1.Text = "Version 1.31.0";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(378, 416);
+            label2.Name = "label2";
+            label2.Size = new Size(165, 80);
+            label2.TabIndex = 2;
+            label2.Text = "Albert Frank 2023-3794\r\njunior Duran 2023-1146\r\nClary luz 2022-2144\r\n\r\n";
+            // 
             // Acerca_de
             // 
-            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.White;
+            ClientSize = new Size(914, 529);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Controls.Add(button1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Acerca_de";
             Text = "Acerca_de";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private Label label1;
+        private Label label2;
     }
 }
